71b288bd70d1a76edab864ff55e317912c023443
Yuwana Patriawarman <pyoewono@gmail.com>
2020-10-31 10:08:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 print ("Hello World" )
 
 try:
-    r = requests.get ('https://go ogle.com')
+    r = requests.get ('https://google.com')
     print(r.status_code)
     if r.status_code == 200:
         print(r.text)
